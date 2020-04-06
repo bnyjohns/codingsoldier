1ebdb9d0a36ce24f76132fbfe5bf295de174d131
Boney Johns <bnyjohns@gmail.com>
2020-04-06 23:06:49 +0530

    adding sql scripts to source

---
+++ CodingSoldier/Sql Scripts/CodingSoldier.sql
@@ -0,0 +1,9 @@
+﻿alter table dbo.Posts 
+ADD CONSTRAINT fk_Posts
+foreign key (CategoryId)
+references Categories(CategoryId)
+
+alter table dbo.Studies 
+ADD CONSTRAINT fk_Studies
+foreign key (CategoryId)
+references Categories(CategoryId)
