f6f4a9a5e9e4699260877ec119995c93c92c6eef
Boney Johns <bnyjohns@gmail.com>
2020-04-07 22:29:47 +0530

    content update

--- CodingSoldier/Core/Entities/Certification.cs
+++ CodingSoldier/Core/Entities/Certification.cs
@@ -9,6 +9,7 @@ namespace CodingSoldier.Core.Entities
     {
         public string Name { get; set; }
         public string UrlPath { get; set; }
+        public string ExternalUrl { get; set; }
 
         public static List<Certification> GetCertifications()
         {
@@ -16,6 +17,7 @@ namespace CodingSoldier.Core.Entities
             list.Add(new Certification { Name = "Developing ASP.NET MVC 4 Web Applications", UrlPath = "developing-asp-net-mvc-4-web-applications" });
             list.Add(new Certification { Name = "AWS Certified Developer - Associate", UrlPath = "aws-certified-developer-associate" });
             list.Add(new Certification { Name = "AWS Certified Solution Architect - Associate", UrlPath = "aws-certified-solutions-architect-associate" });
+            list.Add(new Certification { Name = "AZ-301 Microsoft Azure Architect Design", ExternalUrl= "https://www.youracclaim.com/badges/103b8c83-6c94-49ed-9e29-c5304cfedf87" });
             return list;
         }
     }
